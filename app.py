a451190d3053db5600bdf17a6efea2d8942f1fe2
tuyet2614 <nguyenanhtuyet2614@gmail.com>
2022-01-03 22:29:48 +0700

    second commit

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return ('Hello World')
+    return 'Hello'
+
+@app.route('/author')
+def author():
+    #return 'Hi'
+    return render_template('author.html')
 
 if __name__ == '__main__':
     app.run()
+    
